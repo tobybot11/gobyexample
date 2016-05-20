b4c8b8d98fbe5e4ceba04c4be078acf5fb5b9122
Tobias Ford <toby.ford@pobox.com>
2016-05-19 21:13:44 -0400

    main.go for variadic functions

---
+++ variadic/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func sums(nums ...int) {
+	fmt.Print(nums, " ")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+
+}
